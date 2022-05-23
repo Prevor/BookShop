a89781b99ad78e4486ed3169c82da4b91007fc7e
MethaTrader <methatrader.money@gmail.com>
2022-05-23 21:22:58 +0300

    implement mega feature

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -4,15 +4,15 @@
       <v-toolbar class="grey darken-3">
         <v-toolbar-title>
           <v-img
-            src="@/assets/logo.png"
-            max-height="60"
-            max-width="140"
+              src="@/assets/logo.png"
+              max-height="60"
+              max-width="140"
           ></v-img>
         </v-toolbar-title>
         <v-spacer></v-spacer>
         <v-toolbar-items>
-          <v-btn> Авторизація </v-btn>
-          <v-btn> Реєстрація </v-btn>
+          <v-btn> Авторизація</v-btn>
+          <v-btn> Реєстрація</v-btn>
         </v-toolbar-items>
       </v-toolbar>
     </div>
@@ -21,65 +21,77 @@
       <div>
         <v-toolbar class="grey darken-4 d-flex justify-center">
           <v-toolbar-items>
-            <v-card-text> Художня </v-card-text>
-            <v-card-text> Дитяча </v-card-text>
-            <v-card-text> Манґа </v-card-text>
-            <v-card-text> Історія </v-card-text>
-            <v-card-text> Англійська </v-card-text>
+            <v-card-text> Художня</v-card-text>
+            <v-card-text> Дитяча</v-card-text>
+            <v-card-text> Манґа</v-card-text>
+            <v-card-text> Історія</v-card-text>
+            <v-card-text> Англійська</v-card-text>
+
           </v-toolbar-items>
         </v-toolbar>
       </div>
     </template>
- 
-<template>
-  <v-container>
-    <v-row>
-      <template v-for="n in 4">
-        <v-col :key="n" >
-          <v-card  class="pa-2"
-            outlined
-            tile>
-            Книга
-          </v-card>
-        </v-col>
-        <v-responsive
-          v-if="n === 4"
-          :key="`width-${n}`"
-           width="100%"
-        ></v-responsive>
-      </template>
-    </v-row>
-  </v-container>
-</template>
+
+    <template>
+      <v-container>
+        <v-flex d-flex>
+          <v-layout wrap>
+            <v-flex mx-5 mt-5 xl2 lg2 md-4 sm-6 xs-12  v-for="item in books" :key="item.id">
+              <v-card class="card-container">
+                <v-img
+                    height="256"
+                    src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+                ></v-img>
+                <span>{{ item.text }}</span>
+                lorem500
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-flex>
 
 
+        <!--    <v-row>-->
+        <!--      <template v-for="n in 4">-->
+        <!--        <v-col :key="n" >-->
+        <!--          <v-card  class="pa-2"-->
+        <!--            outlined-->
+        <!--            tile>-->
+        <!--            Книга-->
+        <!--          </v-card>-->
+        <!--        </v-col>-->
+        <!--        <v-responsive-->
+        <!--          v-if="n === 4"-->
+        <!--          :key="`width-${n}`"-->
+        <!--           width="100%"-->
+        <!--        ></v-responsive>-->
+        <!--      </template>-->
+        <!--    </v-row>-->
+      </v-container>
+    </template>
 
-    <!-- <template>
-      <v-card class="white mt-16" max-width="200">
-        <h3>Популярні категорії</h3>
-        <v-container>
-          <v-checkbox label="Журнали"> </v-checkbox>
-
-          <v-checkbox> </v-checkbox>
-
-          <v-checkbox> </v-checkbox>
-
-          <v-checkbox> </v-checkbox>
-
-          <v-checkbox> </v-checkbox>
-
-          <v-checkbox> </v-checkbox>
-
-          <v-checkbox> </v-checkbox>
-        </v-container>
-      </v-card>
-    </template> -->
 
   </v-app>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'App',
+
+  data: () => ({
+    books: [
+      { image: "", text: "Book 1", link: ""},
+      { image: "", text: "Book 2", link: ""},
+      { image: "", text: "Book 3", link: ""},
+      { image: "", text: "Book 4", link: ""},
+      { image: "", text: "Book 4", link: ""},
+      { image: "", text: "Book 5", link: ""},
+      { image: "", text: "Book 6", link: ""},
+      { image: "", text: "Book 7", link: ""},
+      { image: "", text: "Book 8", link: ""},
+      { image: "", text: "Book 9", link: ""},
+    ]
+  }),
+};
 </script>
 
 <style>
@@ -90,11 +102,11 @@ export default {};
 }
 
 .v-toolbar__items {
-    display: flex;
-    height: inherit;
-    color: white!important;
-    gap: 2em;
-    flex-wrap: nowrap;
+  display: flex;
+  height: inherit;
+  color: white !important;
+  gap: 2em;
+  flex-wrap: nowrap;
 }
 
 </style>
